0fe958007bd0713c529f74f4df1c6ebc4a9a4807
alinaved <navedalimail@gmail.com>
2017-07-14 00:18:13 +0100

    Added few more examples

--- pyconcepts/general/decorator.py
+++ pyconcepts/general/decorator.py
@@ -3,13 +3,64 @@ Created on Feb 4, 2014
 
 @author: Naved
 '''
-# its a function working as a decorator
-def dec_test(test_meth):
-    return test_meth(1)
-@dec_test
-def my_meth(number):
-    return number+1
+from functools import wraps
 
-# print my_meth(1)
-    
-dec_test(my_meth())
+def decorator(func):
+    """
+    :param func:
+    :return: func result after calling it with 1
+    """
+    return func(1)
+
+#Below statement is equal to decorator( decoratee )
+@decorator
+def decoratee(number):
+    """
+    :param number:
+    :return: number + 1
+    """
+    return number + 1
+
+from functools import wraps
+def memoize(func):
+    cache = {}
+    @wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            print 'Running func'
+            cache[args] = func(*args)
+        else:
+            print 'result in cache'
+        return cache[args]
+    return wrap
+
+@memoize
+def myfunc(a):
+    return a**2
+
+def decorator2(func):
+    """
+    :param func:
+    :return: function wrapper
+    """
+    def wrapper(number):
+        number = number + 1
+        return func(number)
+    return wrapper
+
+@decorator2
+def decoratee2(number):
+    """
+    :param number:
+    :return: number + 1
+    """
+    return number + 1
+
+
+
+if __name__ == "__main__":
+
+    print decoratee2(1)
+#>>> 3
+    print decoratee2(2)
+#>>> 4
--- pyconcepts/oop/property_test.py
+++ pyconcepts/oop/property_test.py
@@ -4,46 +4,93 @@ Created on 04-Feb-2014
 @author: naved
 '''
 ''
-#A way to provide access methods to change any attribute of a class
+# A way to provide access methods to change any attribute of a class
 # There are inheritence friendly 
- 
+import abc
+
+class MyPropertyValidation(object):
+    def __init__(self):
+        self.__age = None
+
+    @property
+    def age(self):
+        if not self.__age:
+            raise ValueError("Age is None and needs to be set first.")
+        return self.__age
+
+    @age.setter
+    def age(self, val):
+        if isinstance( val, int ) and val > 0:
+            self.__age = val
+        else:
+            raise ValueError("Age needs to be an integer and greater than zero")
+
+
+
 class MyProperty(object):
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.__name = name
+
     @property
     def xname(self):
-        return self.name
-    
+        return self.__name
+
     @xname.setter
-    def xname(self,val):
-        self.name=val
-    
+    def xname(self, val):
+        self.__name = val
+
     @xname.deleter
     def xname(self):
-        del self.name
-
+        del self.__name
 
 class MyOtherProperty(object):
-    def __init__(self,name):
-        self.name=name
-    
+    def __init__(self, name):
+        self.name = name
+
     def getXName(self):
-        return self.name    
-    
-    def setXName(self,val):
-        self.name=val
-    
-    
-    def deleteXName(self):        
+        return self.name
+
+    def setXName(self, val):
+        self.name = val
+
+    def deleteXName(self):
         del self.name
- 
-        
-    xname=property(getXName,setXName,deleteXName,"here is all the doc you want")
+
+    xname = property(getXName, setXName, deleteXName, "here is all the doc you want")
 
 
-mo=MyOtherProperty('naved')
-print mo.xname
-mo.xname="test"
-print mo.xname
-del mo.xname
-print mo.xname
+class MyAbstractProperty(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def xname(self): pass
+
+
+class MyAbstractPropertySubclass(MyAbstractProperty):
+    @property
+    def xname(self):
+        return 1
+
+
+class MyFinalProperty(object):
+
+    @property
+    def name(self):
+        return "John"
+
+if __name__ == '__main__':
+    # #Instantiate class with initial value
+    # myProp = MyProperty("John")
+    # #Print initial value
+    # print myProp.xname
+    # #Update value
+    # myProp.xname = "Doe"
+    # #Print updated value
+    # print myProp.xname
+    # #Delete property
+    # del myProp.xname
+    # #This will raise exception
+    # print myProp.xname
+    m =MyFinalProperty()
+    print m.name
+    m.name = "new"
